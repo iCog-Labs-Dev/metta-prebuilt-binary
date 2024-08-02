4aebf4e4dfda319e4150c3b2d4560feff9d7e91f
ishak <ishaksebsib@gmail.com>
2024-08-02 11:33:18 +0300

    [ENH] tree formatter Added

---
+++ tools/formatter/tree-formater.py
@@ -0,0 +1,89 @@
+import argparse
+
+from hyperon import MeTTa
+
+
+class TreeFormatter:
+    def __init__(self):
+        self.metta = MeTTa()
+        self.metta.run(
+            """
+            ;; -----------------------------------
+            ;; -------- a function to get the right or left child
+            ;; -----------------------------------
+
+            (: getChild (-> Tree Location Tree))
+            (= (getChild (TreeNode $nodeValue $guardSet Nil) $opt) Nil)
+            (= (getChild (TreeNode $nodeValue $guardSet (Cons $l $xs)) L) $l)
+            (= (getChild (TreeNode $nodeValue $guardSet (Cons $l (Cons $r $xs))) R) $r)
+            """
+        )
+
+    def remove_brackets(self, input):
+        return str(input).replace("[", "").replace("]", "")
+
+    def get_right_child(self, input):
+        input = self.remove_brackets(str(input))
+        return self.remove_brackets(
+            self.metta.run(
+                f"""
+        !(getChild {input} R)
+        """
+            )
+        )
+
+    def get_left_child(self, input):
+        input = self.remove_brackets(str(input))
+        return self.remove_brackets(
+            self.metta.run(
+                f"""
+        !(getChild {input} L)
+        """
+            )
+        )
+
+    def simplify_tree_node(self, input):
+        simplified_string = str(input).split(")")[0] + ")"
+        return simplified_string
+
+    def print_tree(self, input, indent=0):
+        if input == "Nil":
+            return
+
+        current_node = self.simplify_tree_node(input)
+
+        left_child = self.get_left_child(input)
+        right_child = self.get_right_child(input)
+
+        if indent == 0:
+            is_or_node = "OR" in current_node
+            print("TreeNode " + ("OR" if is_or_node else "AND"))
+        else:
+            print("─" * indent + " " + str(current_node))
+
+        if left_child != "Nil" or right_child != "Nil":
+            print(" " * (indent + 2) + "├──Left  ", end="")
+            if left_child != "Nil":
+                self.print_tree(left_child, indent + 4)
+
+            print(" " * (indent + 2) + "└──Right ", end="")
+            if right_child != "Nil":
+                self.print_tree(right_child, indent + 4)
+
+
+class CLIFormatter:
+    def __init__(self):
+        self.formatter = TreeFormatter()
+
+    def main(self):
+        parser = argparse.ArgumentParser(
+            description="Format and print a tree structure"
+        )
+        parser.add_argument("input", help="The input tree to be formatted")
+        args = parser.parse_args()
+        self.formatter.print_tree(self.formatter.remove_brackets(args.input))
+
+
+if __name__ == "__main__":
+    app = CLIFormatter()
+    app.main()
