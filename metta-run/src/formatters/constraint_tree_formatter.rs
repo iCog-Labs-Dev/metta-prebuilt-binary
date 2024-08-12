aeeca76994b65452dfc347085d9afff1e248e07a
ishak <ishaksebsib@gmail.com>
2024-08-12 10:33:13 +0300

    [ENH] handle the case where mettaout put is constriant tree

--- metta-run/src/formatters/constraint_tree_formatter.rs
+++ metta-run/src/formatters/constraint_tree_formatter.rs
@@ -7,10 +7,24 @@ pub fn format(metta_output: String) {
         env::var("HOME").unwrap()
     );
 
+    let binary_tree_checker_path = format!(
+        "{}/metta-bin/tools/utils/binary_tree_checker.py",
+        env::var("HOME").unwrap()
+    );
+
     //check if there are tree in metta output if there is tree format them
     for line in metta_output.lines() {
         if line.starts_with("[(TreeNode") {
-            runners::python::run(&formatter_path, &line.to_string());
+            let is_binary_tree = runners::python::run(
+                &binary_tree_checker_path,
+                &line.to_string().replace("[", "").replace("]", ""),
+            );
+            if is_binary_tree == "False\n" {
+                let formatted_tree = runners::python::run(&formatter_path, &line.to_string());
+                println!("{}", formatted_tree);
+                continue;
+            }
+            println!("{}", line);
         } else {
             println!("{}", line);
         }
