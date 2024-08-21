a70fcac3edba6d72a5638663320d5d292f7438ba
amanuel-1 <amanuelgaromsa@gmail.com>
2024-08-21 11:24:55 +0300

    colorized general output formater

--- metta-run/src/formatters/commands.rs
+++ metta-run/src/formatters/commands.rs
@@ -1,6 +1,6 @@
 use clap::Subcommand;
 
-use super::{binary_tree_formatter, constraint_tree_formatter, guardset_tree_formatter};
+use super::{binary_tree_formatter, constraint_tree_formatter, output_formatter};
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum FormatterCommands {
@@ -14,7 +14,9 @@ pub enum FormatterCommands {
     Fgt,
 }
 
-pub fn format(metta_output: String, command: FormatterCommands) {
+pub fn format(metta_output: (String, String), command: FormatterCommands) {
+    let (ref metta_output_stderr, ref metta_output_str) = metta_output;
+    let metta_augmented = format!("{}{}", metta_output_stderr, metta_output_str);
     match command {
         FormatterCommands::Fbt => binary_tree_formatter::format(metta_output),
         FormatterCommands::Fct => constraint_tree_formatter::format(metta_output),
